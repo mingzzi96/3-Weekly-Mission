a14af68989ccbabe7741f52a9492dc2c02896344
mingming <jimin23623@gmail.com>
2023-12-04 13:54:37 +0900

    :heavy_plus_sign:chore: eslint/prettier 설정 추가

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function solution(sides) {
-    var answer = 0;
-    console.log(sides.sort((a,b) => a-b))
-    return answer;
+function solution(arr, idx) {
+    for(i=idx; i < arr.length; i++){
+        const nowNum = arr[i]
+        if (nowNum === 1) return i
+    }
+    return -1
 }
 
-solution([1, 2, 3]);
+solution([0, 0, 0, 1], 1)
