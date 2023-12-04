0b7a9bd6a695ab378b0c546b49c9ad025ff5afce
mingming <jimin23623@gmail.com>
2023-12-04 14:06:38 +0900

    :memo:docs: EOL 추가

--- script.js
+++
@@ -1,9 +0,0 @@
-function solution(arr, idx) {
-    for(i=idx; i < arr.length; i++){
-        const nowNum = arr[i]
-        if (nowNum === 1) return i
-    }
-    return -1
-}
-
-solution([0, 0, 0, 1], 1)
