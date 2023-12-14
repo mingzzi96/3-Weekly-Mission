87f0be6ee3264ca64edbc72c36d5b4430e66c309
mingming <jimin23623@gmail.com>
2023-12-14 17:55:13 +0900

    :sparkles:feat: 이메일 에러 메시지 textContent로 넣어주는걸로 수정

--- script/signin.js
+++ script/signin.js
@@ -5,11 +5,15 @@ const pwErrorMessage = document.querySelector("#pw_error");
 
 const emailFocusOutCheck = (e) => {
     let target = e.target;
+
+    // email 입력이 되어있지 않은 경우 에러 표시
     if (target.value.length <= 0) {
         emailInputEl.classList.add("error");
+        emailErrorMessage.textContent = "이메일을 입력해주세요.";
         emailErrorMessage.classList.add("show");
     } else {
         emailInputEl.classList.remove("error");
+        emailErrorMessage.textContent = "";
         emailErrorMessage.classList.remove("show");
     }
 };
--- script/signup.js
+++ script/signup.js
@@ -7,11 +7,15 @@ const pwConfirmErrorMessage = document.querySelector("#pwConfirm_error");
 
 const emailFocusOutCheck = (e) => {
     let target = e.target;
+
+    // email 입력이 되어있지 않은 경우 에러 표시
     if (target.value.length <= 0) {
         emailInputEl.classList.add("error");
+        emailErrorMessage.textContent = "이메일을 입력해주세요.";
         emailErrorMessage.classList.add("show");
     } else {
         emailInputEl.classList.remove("error");
+        emailErrorMessage.textContent = "";
         emailErrorMessage.classList.remove("show");
     }
 };
