bc689d3720613e63b4810a9df1dad56a5e4a1ead
mingming <jimin23623@gmail.com>
2023-12-19 00:56:33 +0900

    :recycle:refactor: error메시지 관련한것들 함수로 만들기 및 early return

--- script/sign/signin.js
+++ script/sign/signin.js
@@ -11,81 +11,91 @@ let passwordValid = false;
 
 const handleEmailFocusOut = (e) => {
     // 이메일 focus out 기능 구현
-    const target = e.target;
-    const targetValue = target.value;
-    if (target.value.length <= 0) {
-        // email 입력이 되어있지 않은 경우 에러 표시
-        emailErrorMessage.textContent = "이메일을 입력해주세요.";
-        emailValid = false;
-    } else if (emailRegex.test(targetValue) === false) {
-        // 이메일 형식이 아닐 경우 에러 표시
-        emailErrorMessage.textContent = "올바른 이메일 주소가 아닙니다.";
-        emailValid = false;
-    } else {
-        emailValid = true;
-    }
+    const emailValue = e.target.value;
 
-    // 중복되는 css 제어 한 블럭에 모으기
-    if (!emailValid) {
-        emailInputElement.classList.add("error");
-        emailErrorMessage.classList.add("show");
-    } else {
-        emailInputElement.classList.remove("error");
-        emailErrorMessage.textContent = "";
-        emailErrorMessage.classList.remove("show");
+    try {
+        if (emailValue.length <= 0) {
+            // email 입력이 되어있지 않은 경우 에러 표시
+            throw new Error("이메일을 입력해주세요.");
+        }
+
+        if (!emailRegex.test(emailValue)) {
+            // 이메일 형식이 아닐 경우 에러 표시
+            throw new Error("올바른 이메일 주소가 아닙니다.");
+        }
+        hideEmailError();
+    } catch (error) {
+        showEmailError(error.message);
     }
+};
+
+const showEmailError = (message) => {
+    emailValid = false;
+    emailInputElement.classList.add("error");
+    emailErrorMessage.classList.add("show");
+    emailErrorMessage.textContent = message;
+};
+
+const hideEmailError = () => {
+    emailValid = true;
+    emailInputElement.classList.remove("error");
+    emailErrorMessage.classList.remove("show");
+    emailErrorMessage.textContent = "";
 };
 
 const handlePasswordFocusOut = (e) => {
     // 비밀번호 focus out 기능 구현
-    const target = e.target;
-    if (target.value.length <= 0) {
-        // 비밀번호 값이 없을 경우 에러 표시
-        passwordErrorMessage.textContent = "비밀번호를 입력해주세요.";
-        passwordValid = false;
-    } else {
-        passwordValid = true;
+    const passwordValue = e.target.value;
+    try {
+        if (passwordValue.length <= 0) {
+            // 비밀번호 값이 없을 경우 에러 표시
+            throw new Error("비밀번호를 입력해주세요.");
+        }
+        hidePasswordError();
+    } catch (error) {
+        showPasswordError(error.message);
     }
+};
 
-    // 중복되는 css 제어 한 블럭에 모으기
-    if (!passwordValid) {
-        passwordInputElement.classList.add("error");
-        passwordErrorMessage.classList.add("show");
-    } else {
-        passwordInputElement.classList.remove("error");
-        passwordErrorMessage.textContent = "";
-        passwordErrorMessage.classList.remove("show");
-    }
+const showPasswordError = (message) => {
+    passwordValid = false;
+    passwordInputElement.classList.add("error");
+    passwordErrorMessage.classList.add("show");
+    passwordErrorMessage.textContent = message;
+};
+
+const hidePasswordError = () => {
+    passwordValid = true;
+    passwordInputElement.classList.remove("error");
+    passwordErrorMessage.classList.remove("show");
+    passwordErrorMessage.textContent = "";
 };
 
 const handleSubmit = (e) => {
     // submit 버튼 기능 구현
     e.preventDefault();
-    try {
-        if (!emailValid) {
-            // emailValid가 false일때 동작
-            emailInputElement.focus();
-            emailInputElement.classList.add("error");
-            emailErrorMessage.textContent = "이메일을 확인해 주세요.";
-            emailErrorMessage.classList.add("show");
-            emailValid = false;
-            return;
-        }
+    if (!emailValid) {
+        // emailValid가 false일때 동작
+        emailInputElement.focus();
+        emailInputElement.classList.add("error");
+        emailErrorMessage.textContent = "이메일을 확인해 주세요.";
+        emailErrorMessage.classList.add("show");
+    }
 
-        if (!passwordValid) {
-            // passwordValid가 false일때 동작
-            passwordInputElement.focus();
-            passwordInputElement.classList.add("error");
-            passwordErrorMessage.textContent = "비밀번호를 확인해 주세요.";
-            passwordErrorMessage.classList.add("show");
-            passwordValid = false;
-            return;
-        }
+    if (!passwordValid) {
+        // passwordValid가 false일때 동작
+        passwordInputElement.focus();
+        passwordInputElement.classList.add("error");
+        passwordErrorMessage.textContent = "비밀번호를 확인해 주세요.";
+        passwordErrorMessage.classList.add("show");
+    }
+
+    if (
+        emailInputElement.value === "test@codeit.com" &&
+        passwordInputElement.value === "codeit101"
+    ) {
         // 문제 없다면 페이지 이동시킴
         e.currentTarget.submit();
-    } catch (error) {
-        // error
-        console.log(error);
     }
 };
 
--- script/sign/signup.js
+++ script/sign/signup.js
@@ -14,126 +14,131 @@ let passwordValid = false;
 let passwordConfirmValid = true;
 
 const handleEmailFocusOut = (e) => {
-    // 이메일 focus out 기능 구현
-    const target = e.target;
-    const targetValue = target.value;
+    const emailValue = e.target.value;
 
-    if (target.value.length <= 0) {
-        // email 입력이 되어있지 않은 경우 에러 표시
-        emailErrorMessage.textContent = "이메일을 입력해주세요.";
-        emailValid = false;
-    } else if (emailRegex.test(targetValue) === false) {
-        // 이메일 형식이 아닐 경우 에러 표시
-        emailErrorMessage.textContent = "올바른 이메일 주소가 아닙니다.";
-        emailValid = false;
-    } else if (targetValue === "test@codeit.com") {
-        // test@codeit.com라면 이미 사용중인 이메일 표시
-        emailErrorMessage.textContent = "이미 사용 중인 이메일입니다.";
-        emailValid = false;
-    } else {
-        emailValid = true;
+    try {
+        if (emailValue.length === 0) {
+            throw new Error("이메일을 입력해주세요.");
+        }
+        if (!emailRegex.test(emailValue)) {
+            throw new Error("올바른 이메일 주소가 아닙니다.");
+        }
+        if (emailValue === "test@codeit.com") {
+            throw new Error("이미 사용중인 이메일 주소입니다.");
+        }
+        hideEmailError();
+    } catch (error) {
+        showEmailError(error.message);
     }
+};
 
-    // 중복되는 css 제어 한 블럭에 모으기
-    if (!emailValid) {
-        emailInputElement.classList.add("error");
-        emailErrorMessage.classList.add("show");
-    } else {
-        emailInputElement.classList.remove("error");
-        emailErrorMessage.textContent = "";
-        emailErrorMessage.classList.remove("show");
-    }
+const showEmailError = (message) => {
+    emailValid = false;
+    emailInputElement.classList.add("error");
+    emailErrorMessage.classList.add("show");
+    emailErrorMessage.textContent = message;
+};
+
+const hideEmailError = () => {
+    emailValid = true;
+    emailInputElement.classList.remove("error");
+    emailErrorMessage.classList.remove("show");
+    emailErrorMessage.textContent = "";
 };
 
 const handlePasswordFocusOut = (e) => {
-    // 비밀번호 focus out 기능 구현
-    const target = e.target;
-    if (target.value.length <= 0) {
-        // 비밀번호 값이 없을 경우 에러 표시
-        passwordErrorMessage.textContent = "비밀번호를 입력해주세요.";
-        passwordValid = false;
-    } else if (passwordRegex.test(target.value) === false) {
-        // 영문, 숫자 조합 8자 이상의 값이 아닐 경우 에러 표시
-        passwordInputElement.classList.add("error");
-        passwordErrorMessage.textContent =
-            "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
-        passwordErrorMessage.classList.add("show");
-        passwordValid = false;
-    } else {
-        passwordValid = true;
-    }
+    const passwordValue = e.target.value;
 
-    // 중복되는 css 제어 코드 한 블럭에 모으기
-    if (!passwordValid) {
-        passwordInputElement.classList.add("error");
-        passwordErrorMessage.classList.add("show");
-    } else {
-        passwordInputElement.classList.remove("error");
-        passwordErrorMessage.textContent = "";
-        passwordErrorMessage.classList.remove("show");
+    try {
+        if (passwordValue.length === 0) {
+            throw new Error("비밀번호를 입력해주세요.");
+        }
+
+        if (passwordRegex.test(passwordValue) === false) {
+            throw new Error(
+                "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요."
+            );
+        }
+        hidePasswordError();
+    } catch (error) {
+        showPasswordError(error.message);
     }
 };
 
-const handlePasswordConfirmFocusOut = (e) => {
-    const target = e.target;
-    if (target.value.length <= 0) {
-        // 비밀번호 확인 값이 없을 경우 에러 표시
-        passwordConfirmErrorMessage.textContent = "비밀번호를 확인해주세요.";
-        passwordConfirmValid = false;
-    } else {
-        passwordConfirmValid = true;
-    }
+const showPasswordError = (message) => {
+    passwordValid = false;
+    passwordInputElement.classList.add("error");
+    passwordErrorMessage.classList.add("show");
+    passwordErrorMessage.textContent = message;
+};
 
-    // 중복되는 css 제어 코드 한 블럭에 모으기
-    if (!passwordConfirmValid) {
-        passwordConfirmInputElement.classList.add("error");
-        passwordConfirmErrorMessage.classList.add("show");
-    } else {
-        passwordConfirmInputElement.classList.remove("error");
-        passwordConfirmErrorMessage.classList.remove("show");
+const hidePasswordError = () => {
+    passwordValid = true;
+    passwordInputElement.classList.remove("error");
+    passwordErrorMessage.classList.remove("show");
+    passwordErrorMessage.textContent = "";
+};
+
+const handlePasswordConfirmFocusOut = (e) => {
+    const passwordConfirmValue = e.target.value;
+
+    try {
+        if (passwordConfirmValue.length === 0) {
+            throw new Error("비밀번호를 입력해주세요.");
+        }
+
+        if (passwordRegex.test(passwordConfirmValue) === false) {
+            throw new Error("비밀번호가 일치하지 않습니다.");
+        }
+        hidePasswordConfirmError();
+    } catch (error) {
+        showPasswordConfirmError(error.message);
     }
+};
+
+const showPasswordConfirmError = (message) => {
+    passwordConfirmValid = false;
+    passwordConfirmInputElement.classList.add("error");
+    passwordConfirmErrorMessage.classList.add("show");
+    passwordConfirmErrorMessage.textContent = message;
+};
+
+const hidePasswordConfirmError = () => {
+    passwordConfirmValid = true;
+    passwordConfirmInputElement.classList.remove("error");
+    passwordConfirmErrorMessage.classList.remove("show");
+    passwordConfirmErrorMessage.textContent = "";
 };
 
 const handleSubmit = (e) => {
     // submit 버튼 기능 구현
     e.preventDefault();
-    try {
-        if (!emailValid) {
-            // emailValid가 false일때 동작
-            emailInputElement.focus();
-            emailInputElement.classList.add("error");
-            emailErrorMessage.textContent = "이메일을 확인해 주세요.";
-            emailErrorMessage.classList.add("show");
-            emailValid = false;
-            return;
-        }
-
-        if (!passwordValid) {
-            // passwordValid가 false일때 동작
-            passwordInputElement.focus();
-            passwordInputElement.classList.add("error");
-            passwordErrorMessage.textContent = "비밀번호를 확인해 주세요.";
-            passwordErrorMessage.classList.add("show");
-            passwordValid = false;
-            return;
-        }
-
-        if (!passwordConfirmValid) {
-            // passwordConfirmValid가 false일때 동작
-            passwordConfirmInputElement.focus();
-            passwordConfirmInputElement.classList.add("error");
-            passwordConfirmErrorMessage.textContent =
-                "비밀번호를 확인해 주세요.";
-            passwordConfirmErrorMessage.classList.add("show");
-            passwordConfirmValid = false;
-            return;
-        }
-        // 문제 없다면 페이지 이동시킴
-        e.currentTarget.submit();
-    } catch (error) {
-        // error
-        console.log(error);
+    if (!emailValid) {
+        // emailValid가 false일때 동작
+        emailInputElement.focus();
+        emailInputElement.classList.add("error");
+        emailErrorMessage.textContent = "이메일을 확인해 주세요.";
+        emailErrorMessage.classList.add("show");
     }
+
+    if (!passwordValid) {
+        // passwordValid가 false일때 동작
+        passwordInputElement.focus();
+        passwordInputElement.classList.add("error");
+        passwordErrorMessage.textContent = "비밀번호를 확인해 주세요.";
+        passwordErrorMessage.classList.add("show");
+    }
+
+    if (!passwordConfirmValid) {
+        // passwordConfirmValid가 false일때 동작
+        passwordConfirmInputElement.focus();
+        passwordConfirmInputElement.classList.add("error");
+        passwordConfirmErrorMessage.textContent = "비밀번호를 확인해 주세요.";
+        passwordConfirmErrorMessage.classList.add("show");
+    }
+
+    // 문제 없다면 페이지 이동시킴
+    e.currentTarget.submit();
 };
 
 emailInputElement.addEventListener("focusout", handleEmailFocusOut);
