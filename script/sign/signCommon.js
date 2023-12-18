b4243bd3cffc45ae0f6d6b43251cbc0ba7592b55
mingming <jimin23623@gmail.com>
2023-12-18 14:43:21 +0900

    :recycle:refactor: form method post로 수정

--- script/sign/signCommon.js
+++ script/sign/signCommon.js
@@ -1,25 +1,26 @@
-const formEl = document.querySelector("form");
-const emailInputEl = document.querySelector("#email");
-const pwInputEl = document.querySelector("#password");
+const formElement = document.querySelector("form");
+const emailInputElement = document.querySelector("#email");
+const passwordInputElement = document.querySelector("#password");
 const emailErrorMessage = document.querySelector("#email_error");
-const pwErrorMessage = document.querySelector("#pw_error");
-const pwShowButtonEl = document.querySelector("#eyePw");
-const pwConfirmInputEl = document.querySelector("#password_confirm");
-const pwInputContainer = document.querySelectorAll(".input_password");
-const pwConfirmErrorMessage = document.querySelector("#pwConfirm_error");
-const pwConfirmShowButtonEl = document.querySelector("#eyePwConfirm");
+const passwordErrorMessage = document.querySelector("#pw_error");
+const passwordShowButtonElement = document.querySelector("#eyePw");
+const passwordConfirmInputElement = document.querySelector("#password_confirm");
+const passwordInputContainer = document.querySelectorAll(".input_password");
+const passwordConfirmErrorMessage = document.querySelector("#pwConfirm_error");
+const passwordConfirmShowButtonElement =
+    document.querySelector("#eyePwConfirm");
 
 const emailRegex =
     /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
 const pwRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
 
 // input 눈 아이콘 및 input type 변경 기능
-for (let pwInputContainerItem of pwInputContainer) {
+for (const pwInputContainerItem of pwInputContainer) {
     pwInputContainerItem.addEventListener("click", function (e) {
         if (e.target.classList.contains("eye")) {
             // 클릭된 target이 eye 라는 클래스를 가지고 있을때만 실행
-            let inputType = "password";
-            let selectedInput = e.currentTarget.querySelector("input");
+            const inputType = "password";
+            const selectedInput = e.currentTarget.querySelector("input");
             // 선택된 pwInputContainer에 on 클래스 추가하여 눈 모양 아이콘 제어
             e.currentTarget.classList.toggle("on");
             // 선택된 pwInputContainer에 있는 Input의 type을 수정
@@ -31,15 +32,15 @@ for (let pwInputContainerItem of pwInputContainer) {
 }
 
 export {
-    formEl,
-    emailInputEl,
-    pwInputEl,
+    formElement,
+    emailInputElement,
+    pwInputElement,
     emailErrorMessage,
     pwErrorMessage,
-    pwShowButtonEl,
-    pwConfirmInputEl,
+    pwShowButtonElement,
+    pwConfirmInputElement,
     pwConfirmErrorMessage,
-    pwConfirmShowButtonEl,
+    pwConfirmShowButtonElement,
     pwInputContainer,
     emailRegex,
     pwRegex,
