52e7f385c80de91951415fcba06246482fd65f0c
mingming <jimin23623@gmail.com>
2023-12-31 15:57:40 +0900

    ♻️ REFECTOR : react-router-dom말고 일단 a링크로 넣고 오류 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,16 +15,16 @@ const Footer = () => {
         </a>
       </div>
       <div className="Footer_sns">
-        <a href="https://www.facebook.com/" target="_blank">
+        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
           <img src="./assets/images/facebook.png" alt="facebook" />
         </a>
-        <a href="https://twitter.com/" target="_blank">
+        <a href="https://twitter.com/" target="_blank" rel="noreferrer">
           <img src="./assets/images/twitter.png" alt="twitter" />
         </a>
-        <a href="https://www.youtube.com/" target="_blank">
+        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
           <img src="./assets/images/youtube.png" alt="youtube" />
         </a>
-        <a href="https://www.instagram.com/" target="_blank">
+        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
           <img src="./assets/images/instagram.png" alt="instagram" />
         </a>
       </div>
