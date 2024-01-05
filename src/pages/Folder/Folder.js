2fe83ec6e00036c3cacd02ba077179bd1942f33c
mingming <jimin23623@gmail.com>
2024-01-06 02:04:59 +0900

    ✨ FEAT: folder 페이지 공통 영역 추가

--- src/pages/Folder/Folder.js
+++ src/pages/Folder/Folder.js
@@ -1,9 +1,17 @@
 import FolderAddLinkArea from "@components/FolderAddLinkArea/FolderAddLinkArea";
+import { SearchBar } from "@components/SearchBar";
 
 const Folder = () => {
   return (
     <>
-      <FolderAddLinkArea />
+      <div>
+        <FolderAddLinkArea />
+      </div>
+      <div className="List-MaxWidth" style={{ paddingBottom: `100px` }}>
+        <div className="SearchBarArea" style={{ margin: `40px 0` }}>
+          <SearchBar />
+        </div>
+      </div>
     </>
   );
 };
