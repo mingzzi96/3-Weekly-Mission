cf3ef9e4b2a2ae453cac8af1cb24187a7b4260ce
mingming <jimin23623@gmail.com>
2024-01-06 20:11:31 +0900

    ♻️ REFACTOR: everyTagLi 클릭 함수 안으로 넣어주기

--- src/pages/Folder/Folder.js
+++ src/pages/Folder/Folder.js
@@ -5,16 +5,15 @@ import CardListTitle from "@components/CardListTitle/CardListTitle";
 import "./Folder.css";
 import { useState } from "react";
 
-const everyTagLi = document.querySelectorAll(".sorting-group ul li");
-
 const Folder = () => {
   const [cardListTitle, setCardListTitle] = useState("전체");
   const handleActiveListClick = (e) => {
+    const everyTagLi = document.querySelectorAll(".sorting-group ul li");
     const targetTag = e.target;
     const targetTagLi = targetTag.closest("li");
     const targetTagText = targetTagLi.getAttribute("data-tag");
-    everyTagLi.forEach(function (e) {
-      e.classList.remove("active");
+    everyTagLi.forEach((item) => {
+      item.classList.remove("active"); // 변수명을 event로 수정
     });
     targetTagLi.classList.add("active");
     setCardListTitle(targetTagText);
