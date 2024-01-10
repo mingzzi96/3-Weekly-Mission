de8ddd82a78364c9fd6f244e2364a240a7474bbe
mingming <jimin23623@gmail.com>
2024-01-10 23:18:23 +0900

    ♻️ REFACTOR: api 코드 try catch 없애기

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,14 @@
 const NO_LINK_FOUND = "저장된 링크가 없습니다";
 const FAIL_TO_LOAD_LIST = "리스트를 불러오는데 실패했습니다.";
+const FAIL_TO_LOAD_SAMPLE_USER = "샘플 유저 정보를 불러오는데 실패했습니다.";
+const FAIL_TO_LOAD_USER = "유저 정보를 불러오는데 실패했습니다.";
 
 const API_BASE_URL = "https://bootcamp-api.codeit.kr/api";
 
-export { NO_LINK_FOUND, FAIL_TO_LOAD_LIST, API_BASE_URL };
+export {
+  NO_LINK_FOUND,
+  FAIL_TO_LOAD_LIST,
+  API_BASE_URL,
+  FAIL_TO_LOAD_SAMPLE_USER,
+  FAIL_TO_LOAD_USER,
+};
