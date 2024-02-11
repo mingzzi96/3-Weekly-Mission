00ba131cad33666018ec1baa188f208347247629
mingming <jimin23623@gmail.com>
2024-02-11 23:53:01 +0900

    ✨ feat: title, url에서도 검색이 동시에 될 수 있또록

--- src/utils/filterLinkSearch.js
+++ src/utils/filterLinkSearch.js
@@ -1,6 +1,10 @@
 export function filterLinkSearch(items, keyword) {
+  console.log(items, keyword);
   const lowered = keyword.toLowerCase();
-  return items.filter(({ description }) =>
-    description?.toLowerCase().includes(lowered)
+  return items.filter(
+    ({ description, title, url }) =>
+      description?.toLowerCase().includes(lowered) ||
+      title?.toLowerCase().includes(lowered) ||
+      url?.toLowerCase().includes(lowered)
   );
 }
