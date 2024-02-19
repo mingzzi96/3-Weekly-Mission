fec472809768f45293e3097affe77d3ea460dc90
mingming <jimin23623@gmail.com>
2024-02-19 21:11:08 +0900

    ♻️ refactor: trycatch 사용하여 선언적 코드 짜기

--- api/getFolderData.ts
+++ api/getFolderData.ts
@@ -8,31 +8,34 @@ interface folderData {
 }
 
 export const getFolderData = async ({ keyword, folderId }: folderData = {}) => {
-  let apiUrl = `${API_BASE_URL}/users/1/links`;
+  try {
+    let apiUrl = `${API_BASE_URL}/users/1/links`;
 
-  if (folderId) {
-    apiUrl += `?folderId=${folderId}`;
+    if (folderId) {
+      apiUrl += `?folderId=${folderId}`;
+    }
+
+    const response = await fetch(apiUrl);
+
+    const result = await response.json();
+
+    const transformData = result.data.map((item: CardItem) => ({
+      id: item.id,
+      createdAt: item.created_at,
+      imageSource: item.image_source,
+      description: item.description,
+      url: item.url,
+      title: item.title,
+      folderId: item.folder_id,
+      updatedAt: item.updated_at,
+    }));
+
+    if (!keyword || keyword === null || keyword === "") return transformData;
+
+    return filterLinkSearch(transformData, keyword);
+  } catch (error) {
+    if (error) {
+      throw new Error(`${error} : ${FAIL_TO_LOAD_LIST}`);
+    }
   }
-
-  const response = await fetch(apiUrl);
-
-  if (!response.ok) {
-    throw new Error(FAIL_TO_LOAD_LIST);
-  }
-  const result = await response.json();
-
-  const transformData = result.data.map((item: CardItem) => ({
-    id: item.id,
-    createdAt: item.created_at,
-    imageSource: item.image_source,
-    description: item.description,
-    url: item.url,
-    title: item.title,
-    folderId: item.folder_id,
-    updatedAt: item.updated_at,
-  }));
-
-  if (!keyword || keyword === null || keyword === "") return transformData;
-
-  return filterLinkSearch(transformData, keyword);
 };
--- api/getFolderNameData.ts
+++ api/getFolderNameData.ts
@@ -1,10 +1,13 @@
 import { API_BASE_URL, FAIL_TO_LOAD_LIST } from "@/utils/constants";
 
 export const getFolderNameData = async () => {
-  const response = await fetch(`${API_BASE_URL}/users/1/folders`);
-  if (!response.ok) {
-    throw new Error(FAIL_TO_LOAD_LIST);
+  try {
+    const response = await fetch(`${API_BASE_URL}/users/1/folders`);
+    const result = await response.json();
+    return result.data;
+  } catch (error) {
+    if (error) {
+      throw new Error(`${error} : ${FAIL_TO_LOAD_LIST}`);
+    }
   }
-  const result = await response.json();
-  return result.data;
 };
