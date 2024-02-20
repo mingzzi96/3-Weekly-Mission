0fde9335c8e6e04778dff88f63a42529939dba7e
mingming <jimin23623@gmail.com>
2024-02-20 20:49:12 +0900

    💄 style: 카드 리스트 아이템 hover 스타일 수정

--- components/ui/molecules/card-list/CardList.tsx
+++ components/ui/molecules/card-list/CardList.tsx
@@ -1,14 +1,19 @@
-import { CardItemTransformed } from "@/types/cardItemType";
 import styles from "./CardList.module.css";
+import cardItemStyles from "@/components/ui/atoms/list-card-item/CardItem.module.css";
+import { CardItemTransformed } from "@/types/cardItemType";
 import Card from "../../atoms/list-card-item";
 import Link from "next/link";
 import { Url } from "next/dist/shared/lib/router/router";
+import classNames from "classnames/bind";
 
 const CardList = ({ folderItem }: any) => {
   return (
     <div className={styles.cardListBox}>
       {folderItem.map((cardData: CardItemTransformed) => (
-        <div className={styles.cardListItem} key={cardData.id}>
+        <div
+          className={`${styles.cardListItem} ${cardItemStyles.cardListHover}`}
+          key={cardData.id}
+        >
           <Link href={cardData.url as Url} target="_blank">
             <Card cardData={cardData}>
               <div className={styles.imageBox}>
