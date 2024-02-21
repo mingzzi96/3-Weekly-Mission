21689a4d5308eb864627b2aa512fe3cb1d3f6a06
mingming <jimin23623@gmail.com>
2024-02-21 12:54:59 +0900

    ♻️ refactor: meta tag 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+import Head from "next/head";
 import "../styles/reset.css";
 import "../styles/theme.css";
+import LinkbraryLogo from "@/public/assets/images/logos/logo.svg";
 import Layout from "@/components/Layout";
 import { NextPage } from "next";
 
@@ -10,9 +12,20 @@ interface MyAppProps {
 
 const MyApp = ({ Component, pageProps }: MyAppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta property="og:title" content="Linkbrary" />
+        <meta property="og:url" content="http://localhost:3000/" />
+        <meta property="og:image" content={LinkbraryLogo} />
+        <meta
+          property="og:description"
+          content="세상의 모든 정보를 쉽게 저장하고 관리해 보세요"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 };
 
